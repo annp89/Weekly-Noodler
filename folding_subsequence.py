0ee79a2a4441827b1cdebb84554a8fd134e93167
Ann Paul <ann@prismskylabs.com>
2016-03-24 15:53:19 -0700

    Handle repeating values

--- folding_subsequence.py
+++ folding_subsequence.py
@@ -1,18 +1,20 @@
-input_sequence = [3, 6, 1, 2, 4, 3, 5, 6, 1, 8, 3]
+input_sequence = [1, 2, 2, 5, 3, 4]
 output_sequence = []
 sequence_length = len(input_sequence) - 1
+check = False
+
 
 for count, item in enumerate(input_sequence):
     if count == 0:
         output_sequence.append(item)
-    elif count == 1:
+    elif not check:
         if input_sequence[count] < output_sequence[-1]:
-            if count != sequence_length and input_sequence[count+1] < input_sequence[count]:
+            if count != sequence_length and input_sequence[count+1] <= input_sequence[count]:
                 continue
             check = 'CHECK_GREATER'
             output_sequence.append(item)
         elif input_sequence[count] > output_sequence[-1]:
-            if count != sequence_length and input_sequence[count+1] > input_sequence[count]:
+            if count != sequence_length and input_sequence[count+1] >= input_sequence[count]:
                 continue
             check = 'CHECK_LESSER'
             output_sequence.append(item)
@@ -20,12 +22,12 @@ for count, item in enumerate(input_sequence):
             continue
     else:
         if check == 'CHECK_GREATER' and input_sequence[count] > output_sequence[-1]:
-            if count != sequence_length and input_sequence[count+1] > input_sequence[count]:
+            if count != sequence_length and input_sequence[count+1] >= input_sequence[count]:
                 continue
             check = 'CHECK_LESSER'
             output_sequence.append(item)
         elif check == 'CHECK_LESSER' and input_sequence[count] < output_sequence[-1]:
-            if count != sequence_length and input_sequence[count+1] < input_sequence[count]:
+            if count != sequence_length and input_sequence[count+1] <= input_sequence[count]:
                 continue
             check = 'CHECK_GREATER'
             output_sequence.append(item)
