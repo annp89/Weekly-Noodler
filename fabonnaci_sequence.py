cee714d398ab55e6ad94e8b96209966fc0a20b95
annp89 <annp89@vt.edu>
2016-03-28 21:53:26 -0700

    Week 2

---
+++ fabonnaci_sequence.py
@@ -0,0 +1,17 @@
+from collections import *
+ 
+fib_call_count = defaultdict(int)
+ 
+def fib(n):
+	fib_call_count[n] += 1
+	if n == 0:
+	    return 0
+	elif n == 1:
+	    return 1
+	else:
+	    return fib(n - 1) + fib (n - 2)
+
+def fab(n): 
+	fib(n)
+	for i in xrange(1, n):
+		print fib_call_count[i]
